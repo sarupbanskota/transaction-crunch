9d57f0adb50240b53d6ea97d290aea8140e898a6
Sarup Banskota <sbanskota08@gmail.com>
2015-01-26 10:01:02 +0530

    Improve query script to fit format, explanatory output

--- code/queryscript.rb
+++ code/queryscript.rb
@@ -9,11 +9,14 @@ client = Mysql2::Client.new(
 id_list = client.query("select distinct id from csvdump")
 total_ids = id_list.count
 id_list.each { |row| system "echo #{row["id"]} >> idlisting" }
+index = 1
 File.open("idlisting") do |file|
   file.each_line do |line|
-    item_list = print "#{line}/#{total_ids}" 
+    item_list = print "Querying (#{index}/#{total_ids}). ID #{line}" 
     item_list = client.query("select distinct item from csvdump where id=#{line}")
-    item_list.each { |entry| system "echo #{entry["item"]} >> finallist" }
+    item_list.each { |entry| system "echo -ne ',#{entry["item"]}' >> input_list" }
+    system "echo -n '\n' >> input_list"
+    index+=1
   end
 end
     
